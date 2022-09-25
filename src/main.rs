954caf3663be907834f2737134c8b4438a98d26b
Hector Garza <hggumich@gmail.com>
2022-09-25 10:19:41 -0400

    finish function section

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     another_function_with_parameters(5);
 
     print_labeled_measurement(5, 'h');
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let y = plus_one(5);
+    println!("The value of y: {y}");
 }
 
 fn another_function() {
@@ -18,4 +24,12 @@ fn another_function_with_parameters(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value} {unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(y: i32) -> i32 {
+    y + 1
 }
