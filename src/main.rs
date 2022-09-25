c421b67a0ad84218d2d9a6ebe03cc3aae582db70
Hector Garza <hggumich@gmail.com>
2022-09-25 09:57:41 -0400

    add a function with two parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function();
+
+    another_function_with_parameters(5);
+
+    print_labeled_measurement(5, 'h');
+}
+
+fn another_function() {
+    println!("Another Function");
+}
+
+fn another_function_with_parameters(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value} {unit_label}");
 }
